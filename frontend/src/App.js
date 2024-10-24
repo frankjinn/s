96f03e216a9756fe21f580f1da0f9f32f5eabf34
Frank Jin <frank.jin@berkeley.edu>
2024-10-23 21:15:08 -0700

    making routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
-import Header from "./components/Header";
-// import Todos from "./components/Todos"; // new
+import Todos from "./components/Todos"; // new
 import Dashboard from "./components/Dashboard"; // new
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <ChakraProvider>
-      <Header />
-      <Dashboard /> {/* new */}
+      <BrowserRouter>
+        {/* <Route exact path="/todo" component={Todos} />
+        <Route path="/dashboard" component={Dashboard} /> */}
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
