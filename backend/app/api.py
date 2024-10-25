6f5b77c500c35b7dd6a4fc7ec5b3bbb77a82af4a
Frank Jin <frank.jin@berkeley.edu>
2024-10-24 18:48:17 -0700

    Added backend support for json

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,23 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import json
+import os
 
 
 app = FastAPI()
-todos = [
-    {
-        "id": "1",
-        "item": "Read a book."
-    },
-    {
-        "id": "2",
-        "item": "Cycle around town."
-    }
-]
+todos = [{"id": "1", "item": "Read a book."}, {"id": "2", "item": "Cycle around town."}]
 
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
+# print(os.path.dirname(os.path.realpath(__file__)))
+# with open("./exampleUser.json") as f:
+#     data = json.load(f)
+
+origins = ["http://localhost:3000", "localhost:3000"]
 
 
 app.add_middleware(
@@ -25,7 +19,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
@@ -33,14 +27,22 @@ app.add_middleware(
 async def read_root() -> dict:
     return {"message": todos}
 
+
 @app.get("/todo", tags=["todos"])
 async def get_todos() -> dict:
-    return { "data": todos }
+    return {"data": todos}
+
 
 @app.post("/todo", tags=["todos"])
 async def add_todo(todo: dict) -> dict:
     todos.append(todo)
-    return {
-        "data": { "Todo added." }
-    }
+    return {"data": {"Todo added."}}
 
+
+@app.get("/loadUsers", tags=["loadUsers"])
+async def get_users() -> dict:
+    print(os.path.dirname(os.path.realpath(__file__)))
+    with open("./exampleUser.json") as f:
+        file = json.load(f)
+        print(file)
+    return {"userList": file}
