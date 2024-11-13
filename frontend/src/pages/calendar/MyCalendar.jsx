ad61fbdfe9027fa99f39df4e03bf142ee8e3e166
Frank Jin <frank.jin@berkeley.edu>
2024-11-12 18:26:05 -0800

    calendar updates

--- frontend/src/pages/calendar/MyCalendar.jsx
+++ frontend/src/pages/calendar/MyCalendar.jsx
@@ -1,29 +1,72 @@
+import React, { Component } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
 
 const localizer = momentLocalizer(moment);
-const state = {
-  events: [
-    {
-      start: moment().toDate(),
-      end: moment().add(1, "days").toDate(),
-      title: "Some title",
-    },
-  ],
-};
+const DnDCalendar = withDragAndDrop(Calendar);
+
+// const state = {
+//   events: [
+//     {
+//       start: moment().toDate(),
+//       end: moment().add(1, "days").toDate(),
+//       title: "Some title",
+//     },
+//   ],
+// };
+
+// const onEventResize = (data) => {
+//   const { start, end } = data;
+
+//   setState((state) => {
+//     state.events[0].start = start;
+//     state.events[0].end = end;
+//     return { events: [...state.events] };
+//   });
+// };
+
+// const onEventDrop = (data) => {
+//   console.log(data);
+// };
+
 export default function MyCalendar(props) {
+  const [state, setState] = useState({
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(1, "days").toDate(),
+        title: "Some title",
+      },
+    ],
+  });
+  const onEventResize = (data) => {
+    const { start, end } = data;
+
+    setState((state) => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: [...state.events] };
+    });
+  };
+
+  const onEventDrop = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <Calendar
-        localizer={localizer}
-        defaultDate={new Date()}
+    <div className="App">
+      <DnDCalendar
+        defaultDate={moment().toDate()}
         defaultView="month"
         events={state.events}
-        style={{
-          height: "100vh",
-          width: "100vw",
-        }}
+        localizer={localizer}
+        onEventDrop={onEventDrop}
+        onEventResize={onEventResize}
+        resizable
+        style={{ height: "100vh" }}
       />
     </div>
   );
